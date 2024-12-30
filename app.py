8e733dffa68162ef8363a60be470e3937c8a1f8b
shreya <shreyamandavkar0307@gmail.com>
2024-12-30 15:27:36 +0530

    *num

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ alphabet = 1+2j
 # To take input from the user
 #num = eval(input('Enter a number: '))
 
-num_sqrt = cmath.sqrt(num)
+num_sqrt = cmath.sqrt(num)*(num)
 print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))
